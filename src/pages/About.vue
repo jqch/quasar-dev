3feed2abb3c50fd79e14bad4e38831252d01f42c
Jhonny Quispe <jhonnyq17@gmail.com>
2021-11-12 16:13:05 -0400

    fist commit

---
+++ src/pages/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page padding>
+    <h1>About</h1>
+     <div class="q-gutter-xl">
+      <q-btn color="primary">Uno</q-btn>
+      <q-btn color="primary">Dos</q-btn>
+      <q-btn color="primary">Tres</q-btn>
+    </div>
+    <div class="row">
+      <p class="col-12 col-sm-6 col-md-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid a perspiciatis commodi dicta suscipit provident incidunt consequatur tenetur ad molestias blanditiis qui ullam autem dolores eius veritatis, rem ipsam reprehenderit. Nobis rerum magnam possimus iusto exercitationem reprehenderit molestiae dolorem. Ratione, officia. Quis soluta ut minus fugiat optio architecto maxime accusantium.
+      </p>
+      <p class="col-12 col-sm-6 col-md-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid a perspiciatis commodi dicta suscipit provident incidunt consequatur tenetur ad molestias blanditiis qui ullam autem dolores eius veritatis, rem ipsam reprehenderit. Nobis rerum magnam possimus iusto exercitationem reprehenderit molestiae dolorem. Ratione, officia. Quis soluta ut minus fugiat optio architecto maxime accusantium.
+      </p>
+      <p class="col-12 col-md-6">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid a perspiciatis commodi dicta suscipit provident incidunt consequatur tenetur ad molestias blanditiis qui ullam autem dolores eius veritatis, rem ipsam reprehenderit. Nobis rerum magnam possimus iusto exercitationem reprehenderit molestiae dolorem. Ratione, officia. Quis soluta ut minus fugiat optio architecto maxime accusantium.
+      </p>
+    </div>
+
+    <div class="row items-center bg-primary text-white text-center" style="height:400px">
+      <p class="col bg-positive">Lorem ipsum dolor sit.</p>
+      <p class="col bg-positive">Lorem ipsum dolor sit.</p>
+      <p class="col bg-positive">Lorem ipsum dolor sit.</p>
+    </div>
+  </q-page>
+</template>
